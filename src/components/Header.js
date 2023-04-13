3bbca63afb6f46311f1540cb807d376751257b91
kara4600 <kara178@comcast.net>
2023-04-12 22:22:20 -0700

    Add hyperlink to resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,12 @@ const Header = () => {
         <Link to="/">
           <Button>Home</Button>
         </Link>
+        <Link
+          to="https://github.com/kara4600/kararizzardi.com/blob/b05ac5e7ccb6ae5d0e0017455396f74b04ce47b6/Kara_Rizzardi_Resume.pdf"
+          target={'_blank'}
+        >
+          <Button>Resume{<LaunchIcon />}</Button>
+        </Link>
         <Link to="https://github.com/kara4600/portfolio" target={'_blank'}>
           <Button>Github {<LaunchIcon />}</Button>
         </Link>
